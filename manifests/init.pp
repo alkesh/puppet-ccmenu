d8bc8112bb0e82bf933bca073e00fd555683c491
Alkesh Vaghmaria <alkesh.vaghmaria@wearefriday.com>
2013-07-08 17:21:24 +0100

    Change to hardcoded version of ccmenu and ftp download link

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,6 @@
 class ccmenu {
   package { 'CCMenu':
     provider => 'appdmg',
-    source   => 'http://downloads.sourceforge.net/project/ccmenu/CCMenu/1.6.2/ccmenu-1.6.2-b.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fccmenu%2Ffiles%2Flatest%2Fdownload%3Fsource%3Dfiles&ts=1373297741&use_mirror=kent',
+    source   => 'ftp://ftp.mirrorservice.org/sites/dl.sourceforge.net/pub/sourceforge/c/cc/ccmenu/CCMenu/1.6.2/ccmenu-1.6.2-b.dmg',
   }
 }
--- spec/classes/ccmenu_spec.rb
+++ spec/classes/ccmenu_spec.rb
@@ -4,7 +4,7 @@ describe 'ccmenu' do
   it do
     should contain_package('CCMenu').with(
       provider: 'appdmg',
-      source: 'http://downloads.sourceforge.net/project/ccmenu/CCMenu/1.6.2/ccmenu-1.6.2-b.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fccmenu%2Ffiles%2Flatest%2Fdownload%3Fsource%3Dfiles&ts=1373297741&use_mirror=kent'
+      source: 'ftp://ftp.mirrorservice.org/sites/dl.sourceforge.net/pub/sourceforge/c/cc/ccmenu/CCMenu/1.6.2/ccmenu-1.6.2-b.dmg'
     )
   end
 end
